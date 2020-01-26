f5da6b7c5743b53c12beb52e1f5c9b8b9750ea2d
unknown <nduross1@gmail.com>
2020-01-25 23:54:55 -0800

    made corrections

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const port = process.env.PORT || 5000
 
 
 // process.env.NODE_ENV !== 'production' && require('dotenv').config();
@@ -37,7 +38,7 @@ app.get('/', (req, res) => {
 // routes setup
 app.use('/product', productApi);
 
-app.listen(5000, (err) => {
+app.listen(port, (err) => {
 	if (err) throw err;
 	console.log('=> server now running on port 5000');
 });
